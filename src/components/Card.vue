3e174055bca50d1644f3cd085654c9a1d615aec7
Paul <isakura313@gmail.com>
2020-11-19 13:02:54 +0300

    adasd

---
+++ src/components/Card.vue
@@ -0,0 +1,54 @@
+<template>
+<!--  В template только 1 корневой элемент-->
+<div class="column is-one-quarter">
+  <div class="card">
+    <div class="card__image">
+<!--  v-bind связывает данные с содержимым атрибута-->
+      <img v-bind:src="image">
+    </div>
+    <h3 class="card__title is-size-4"> {{ title }}</h3>
+    <p class="card__price">{{ price }}</p>
+    <p class="category__p">Бестселлер</p>
+    <p class="card__rating">{{rating}} /5</p>
+    <button class="card__button is-danger button is-pulled-right"> Заказать </button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  data() {
+    return {
+      image: 'https://cdn1.ozone.ru/s3/multimedia-t/wc1200/6014496977.jpg',
+      title: 'Название товара',
+      price: 1000,
+      discount: true,
+      newPrice: 900,
+      rating: 4,
+    };
+  },
+};
+
+</script>
+
+<style scoped>
+.card__image{
+  height: 200px;
+  width: auto;
+}
+.card{
+  padding: 20px;
+  margin-bottom: 3em;
+}
+.category__p{
+  color: gold;
+  font-style: italic;
+  font-weight: bold;
+}
+.card__price{
+  color: #c8554f;
+  font-weight: bold;
+  font-size: 22px;
+}
+</style>
---
+++ src/views/Books.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="books">
+    <h1 class="is-size-2 has-text-centered">Страница о книгах</h1>
+    <div class="columns is-multiline card__wrapper">
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from '@/components/Card.vue';
+
+export default {
+  name: 'Books',
+  components: {
+    Card,
+  },
+};
+
+</script>
+
+<style scoped>
+.card__wrapper{
+  margin: 1em 6em;
+}
+</style>
